1def10b1218dbeb1656203a095fa0e03bb771cb9
Squel20 <77104410+Squel20@users.noreply.github.com>
2025-09-11 21:41:11 +0700

    testing weather

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/dashboard";
+import WeatherApp from "./pages/Landing-page";
 
 function ProtectedRoute({ children }) {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
@@ -11,6 +12,7 @@ function ProtectedRoute({ children }) {
 export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<WeatherApp />} />
       <Route path="/login" element={<LoginPage />} />
       <Route
         path="/dashboard"
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api/weather": {
+        target: "https://api.open-meteo.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/weather/, "/v1/forecast"),
+      },
+    },
+  },
 });
